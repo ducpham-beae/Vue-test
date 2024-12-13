eb47bba050a630f82d0b989190ad6e3d040b7874
ducpham-beae <ducpham@beae.com>
2024-12-13 14:56:11 +0700

    Chỉnh sửa UI cho Reset Cart

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -13,7 +13,7 @@ const props = defineProps({
       <h3 class="text-xl font-semibold mb-4">Cart</h3>
       <div v-if="cart.length" class="flex items-center gap-2 mb-4">
         <span class="uppercase">{{props.count}} </span>
-        <span class="ml-auto text-blue-500 flex " @click="props.clear">
+        <span class="ml-auto text-blue-500 fill-blue-500 flex gap-2" @click="props.clear">
           Reset
           <svg class="w-[24px] h-[24px]" clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m3.508 6.726c1.765-2.836 4.911-4.726 8.495-4.726 5.518 0 9.997 4.48 9.997 9.997 0 5.519-4.479 9.999-9.997 9.999-5.245 0-9.553-4.048-9.966-9.188-.024-.302.189-.811.749-.811.391 0 .715.3.747.69.351 4.369 4.012 7.809 8.47 7.809 4.69 0 8.497-3.808 8.497-8.499 0-4.689-3.807-8.497-8.497-8.497-3.037 0-5.704 1.597-7.206 3.995l1.991.005c.414 0 .75.336.75.75s-.336.75-.75.75h-4.033c-.414 0-.75-.336-.75-.75v-4.049c0-.414.336-.75.75-.75s.75.335.75.75z" fill-rule="nonzero"/></svg>
         </span>
